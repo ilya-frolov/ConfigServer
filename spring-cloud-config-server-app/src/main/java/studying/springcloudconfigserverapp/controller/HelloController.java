585d30a11f97b0139cee61e6bf0b94f63ee3c4c5
ilya-frolov <ilyafrolov0711@gmail.com>
2023-05-05 15:47:36 +0300

    REST app

--- spring-cloud-config-server-app/src/main/java/studying/springcloudconfigserverapp/controller/HelloController.java
+++ spring-cloud-config-server-app/src/main/java/studying/springcloudconfigserverapp/controller/HelloController.java
@@ -2,7 +2,6 @@ package studying.springcloudconfigserverapp.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/hello")
 public class HelloController {
 
-    @Value("${message: Hello default}")
+    @Value("${message}")
     String message;
 
     @GetMapping(value = "/message")
